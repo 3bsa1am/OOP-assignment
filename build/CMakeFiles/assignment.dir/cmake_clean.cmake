0559b428fd34ee7697aef0b8df18321fa33b7409
Mohammed <m7md3bs0011@gmail.com>
2025-09-25 03:53:49 +0300

    first commit

---
+++ build/CMakeFiles/assignment.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment.dir/assign.cpp.obj"
+  "CMakeFiles/assignment.dir/assign.cpp.obj.d"
+  "assignment.exe"
+  "assignment.exe.manifest"
+  "assignment.pdb"
+  "libassignment.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
